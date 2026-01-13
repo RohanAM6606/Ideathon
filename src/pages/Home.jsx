af3c508235312010d5bb32d48f99436df2df4911
Rohan A M <rohan02aug@gmail.com>
2026-01-13 08:17:33 +0530

    Fix CI build by adding framer-motion dependency and updating Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,14 @@ import Navbar from '../components/common/Navbar'
 import HeroSection from '../components/hero/HeroSection'
 import About from '../components/about/About'
 import EventDetails from '../components/events/EventDetails'
+
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <About/>
-      <EventDetails/>
+      <About />
+      <EventDetails />
     </div>
   )
 }
