edbe0f76e6e4212814fd276e33f9b212b269bc90
Mayank Thawani <mayankthawani13@gmail.com>
2026-02-01 18:49:16 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Registration from './pages/Registration' // if you have a registration page
+import Registration from './pages/Registration'
 
 function App() {
   return (
